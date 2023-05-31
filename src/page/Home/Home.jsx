14a9150c00cad7882bf9def0495244ce069aa9b1
Nguyen Trong Phuc <nguyentrongphuc1804@gmail.com>
2023-05-31 10:47:41 +0700

    change font size mobile

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -39,7 +39,7 @@ function Home() {
       <div className="body px-[4%]">
         <div className="mt-[80px]">
           <div className="flex items-center justify-between">
-            <div className="relative px-5 py-2 text-3xl font-bold my-5 text-white">
+            <div className="relative px-5 py-2 text-2xl font-bold my-5 text-white">
               <div className="absolute top-0 left-0 w-[4px] h-full bg-red-700"></div>
               <p>Phim Lẻ</p>
             </div>
@@ -60,7 +60,7 @@ function Home() {
         </div>
         <div className="mt-[80px]">
           <div className="flex items-center justify-between">
-            <div className="relative px-5 py-2 text-3xl font-bold my-5 text-white">
+            <div className="relative px-5 py-2 text-2xl font-bold my-5 text-white">
               <div className="absolute top-0 left-0 w-[4px] h-full bg-red-700"></div>
               <p>Phim Bộ</p>
             </div>
@@ -80,7 +80,7 @@ function Home() {
         </div>
         <div className="mt-[80px]">
           <div className="flex items-center justify-between">
-            <div className="relative px-5 py-2 text-3xl font-bold my-5 text-white">
+            <div className="relative px-5 py-2 text-2xl font-bold my-5 text-white">
               <div className="absolute top-0 left-0 w-[4px] h-full bg-red-700"></div>
               <p>Phim Hoạt Hình</p>
             </div>
@@ -103,7 +103,7 @@ function Home() {
         <CardBanner film={filmNew[1]} />
       </div>
       <div className="mt-[80px]">
-        <div className="relative px-5 py-2 text-3xl font-bold my-5 text-white">
+        <div className="relative px-5 py-2 text-2xl font-bold my-5 text-white">
           <p className="text-center">FLIX PRICING</p>
         </div>
         <SlidePricing />
